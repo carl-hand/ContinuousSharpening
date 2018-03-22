78337c328b77f0fe7206cbd4e79c4fcece9cc3c4
carl.hand <carl.a.hand92@gmail.com>
2018-03-22 14:46:04 +0000

    altered exercise 4

---
+++ src/exercises/exercise4/exercise4.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+/**
+ * Exercise 4:
+ * Create a dropdown component which will add an option, with the name
+ * the user has entered into the input box, when your button is clicked
+ */
