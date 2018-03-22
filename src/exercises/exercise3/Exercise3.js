1887756b9d3b3e9e0bfda05eca889397d1dbb148
carl.hand <carl.a.hand92@gmail.com>
2018-03-22 14:51:53 +0000

    altered exercise 3

--- src/exercises/exercise3/Exercise3.js
+++ src/exercises/exercise3/Exercise3.js
@@ -4,5 +4,6 @@
  * re visit the button component you created in exercise 1.
  * Add an onClick handler to this button
  * When the user clicks the button, the value entered into
- * your input component should print out to the console
+ * your input component should display as a new title component
+ * below the input box
  */
